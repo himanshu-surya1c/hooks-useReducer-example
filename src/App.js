c9be17bac8deecef7437a183c8b243c1862d92b1
Himanshu Suryawanshi <62571754+himanshu-surya1c@users.noreply.github.com>
2022-05-26 15:37:59 +0530

    performs actions using useState... added button -  onclick

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import "./styles.css";
+import React, { useState } from "react";
 
 export default function App() {
+  const [count, setCount] = useState(0);
+  const [showText, setShowText] = useState(true);
   return (
     <div className="App">
       <h1>Hooks- useReducer Example</h1>
+      {count}
+
       <h2>Start editing to see some magic happen!</h2>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+          setShowText(!showText);
+          console.log(showText);
+        }}
+      >
+        Click here
+      </button>
+      {showText && <p> This is a text</p>}
     </div>
   );
 }
